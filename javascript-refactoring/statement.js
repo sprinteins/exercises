77cb47a01144e37a66e2f97f7f9f4c2b649ce371
Nima <nima.nowroozi@studysmarter.de>
2023-01-20 14:34:50 +0100

    Put the calculation into a separate function

--- javascript-refactoring/statement.js
+++ javascript-refactoring/statement.js
@@ -6,7 +6,6 @@ const formatCurrency = value => {
         }).format(value / 100);
     }
 
-
 const statement = (invoice, plays) => {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -15,25 +14,29 @@ const statement = (invoice, plays) => {
     for (const performance of invoice.performances) {
         if(!(performance.playID in plays)) throw new Error(`unknown playID for: ${performance.playID}`)
         const play = plays[performance.playID];
-        let thisAmount = play.amount.total;
-        if (performance.audience > play.audience) {
-            thisAmount += play.amount.constant + (play.amount.multiple1 * (performance.audience - play.audience));
-        }
-        thisAmount += play.amount.multiple2 * performance.audience;
-
-
-        // add volume credits
-        volumeCredits += Math.max(performance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(performance.audience / 10);
-
+        const [ volumeCredit, thisAmount ] = calculator(performance, play);
         // print line for this order
         result += `  ${play.name}: ${formatCurrency(thisAmount / 100)} (${performance.audience} seats)\n`;
         totalAmount += thisAmount;
+        volumeCredits += volumeCredit;
     }
     result += `Amount owed is ${formatCurrency(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+}
+
+const calculator = (performance, play) => {
+    let thisAmount = play.amount.total;
+    if (performance.audience > play.audience) {
+        thisAmount += play.amount.constant + (play.amount.multiple1 * (performance.audience - play.audience));
+    }
+    thisAmount += play.amount.multiple2 * performance.audience;
+
+    // add volume credits
+    let volumeCredit = Math.max(performance.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === play.type) volumeCredit += Math.floor(performance.audience / 10);
+    return [volumeCredit, thisAmount];
 }
 
 export default statement;
